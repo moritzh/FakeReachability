85ad1edf00994bd18c250744eba2a49b9e8948fb
Moritz Haarmann <m.haarmann@gmail.com>
2011-12-03 22:08:37 +0100

    Initial Commit

---
+++ FakeReachability/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FakeReachability
+//
+//  Created by Moritz Haarmann on 03.12.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ FakeReachability/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  FakeReachability
+//
+//  Created by Moritz Haarmann on 03.12.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
